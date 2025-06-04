c7e751b597bc13f33ad36246ce929aea516c1c8c
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-06-04 13:34:53 -0300

    feat: add customers table and update orders schema with foreign key reference

--- app-orders/src/db/schema/orders.ts
+++ app-orders/src/db/schema/orders.ts
@@ -3,6 +3,7 @@ import { timestamp } from "drizzle-orm/pg-core";
 import { pgEnum } from "drizzle-orm/pg-core";
 import { text } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core";
+import { customers } from "./customers.ts";
 
 export const orderStatusEnum = pgEnum("order_status", [
   "pending",
@@ -12,7 +13,9 @@ export const orderStatusEnum = pgEnum("order_status", [
 
 export const orders = pgTable("orders", {
   id: text().primaryKey(),
-  customerId: text().notNull(),
+  customerId: text()
+    .notNull()
+    .references(() => customers.id),
   amount: integer().notNull(),
   status: orderStatusEnum().notNull().default("pending"),
   createdAt: timestamp().defaultNow().notNull(),
--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -7,11 +7,18 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { channels } from "../broker/channels/index.ts";
+import { db } from "../db/client.ts";
+import { schema } from "../db/schema/index.ts";
+import { randomUUID } from "node:crypto";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.get("/health", () => {
   return "OK";
@@ -31,7 +38,16 @@ app.post(
 
     console.log("Creating an order with amount", amount);
 
-    channels.orders.sendToQueue("orders", Buffer.from("Hello World"));
+    channels.orders.sendToQueue(
+      "orders",
+      Buffer.from(JSON.stringify({ amount }))
+    );
+
+    await db.insert(schema.orders).values({
+      id: randomUUID(),
+      customerId: "a6090a7a-8cc5-4b9a-9eb9-6d98d23fb890",
+      amount: amount,
+    });
 
     return reply.status(201).send();
   }
